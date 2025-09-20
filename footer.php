599f39e5c83096c7d92c0b4d410e10474a13d290
Milottty <milotymeri1@gmail.com>
2025-09-20 12:49:15 +0200

    Day 2

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="site-footer">
+<p>&copy; <?php echo date('Y') ?> <?php bloginfo('name') ?> | All rights Reserved
+</p>
+<p><a href="#top" class="back-to-top"></a></p>
+<?php wp_footer(); ?>
+</footer>
+</body>
+</html>
