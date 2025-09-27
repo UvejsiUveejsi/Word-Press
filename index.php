1ce38f3dbe7752b1711ce82eb30d1236aed57f25
UvejsiUveejsi <uvejsborovci1@gmail.com>
2025-09-27 12:50:03 +0200

    une
    une

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
+<main>
+    <h2>welcome</h2>
 
-<h1>Hello, Digital School!</h1>
-
+    <?php if(have_posts()): while (have_posts()): the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content() ?></p>
+    <?php endwhile; endif; ?>
+</main>
 <?php get_footer(); ?>
