f242199e6965cdef6fc0dd5329df8dcada6c5566
UvejsiUveejsi <uvejsborovci1@gmail.com>
2025-11-08 12:48:10 +0100

    uvi

--- functions.php
+++ functions.php
@@ -1,20 +1,26 @@
-<?php
+<?php 
 function ds_style(){
     wp_enqueue_style('digitalschool-style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'ds_style');
 
-
 function ds_menu(){
-    register_nav_menus(
-        array(
-            'primary'=>'Primary Menu'
-        ));
+    register_nav_menus( array(
+    'primary'=>'Primary Menu')
+);
 }
 
 add_action('after_setup_theme', 'ds_menu');
 
+add_action('wp_enqueue_scripts', function(){
+    if(is_page_template('aboutus.php')){
+        wp_enqueue_script(
+            'tailwing-play',
+            'https://cdn.tailwindcss.com'
+        );
+    }
+});
 
 
 ?>
---
+++ team.php
@@ -0,0 +1,16 @@
+<?php
+//Template Name: Team(Posts Category) 
+
+get_header();
+?>
+
+
+<main class="team-page section-pad">
+    <header class="team-intro">
+        <h1 id="heading"><?php the_title(); ?></h1>
+</header>
+
+
+<?php
+get_footer();   
+?>
